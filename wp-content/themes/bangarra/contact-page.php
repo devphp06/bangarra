b21a5496ac7c8f338be40ad1e751c5fe32e2f97b
tarsem <sr.devphp06@gmail.com>
2022-12-02 17:31:40 +0530

    feat(Theme): Contact and declaimer pagestempalte created

---
+++ wp-content/themes/bangarra/contact-page.php
@@ -0,0 +1,52 @@
+<?php 
+/**
+ * Template name: Contact
+ */
+get_header();?>
+
+    <!-- Content -->
+    <div class="contactUs">
+      <div class="common-header-strip">
+        <div class="container">
+            <?php if(get_field('heading')){?><h1 class="banner-heading text-white"><?php echo get_field('heading');?></h1><?php }?>
+        </div>
+      </div>
+      <div class="contact-form">
+        <div class="contact-inner container">
+          <div class="row">
+            <div class="col-12 col-lg-7">
+              <?php if(get_field('form_heading')){?><h3 class="heading-sm"><?php echo get_field('form_heading');?></h3><?php }
+                    $form = get_field('form_shortcode');
+                    if ($form) {?>
+                        <div class="contactForm" id="contactForm">
+                            <?php echo do_shortcode($form); ?>
+                        </div>
+                    <?php } ?>
+            </div>
+            <div class="col-12 col-lg-5 rounded-circle text-center text-lg-end desktop-map">
+            <?php if(get_field('map_source_link')){?><iframe class="rounded-circle" src="<?php echo get_field('map_source_link');?>" width="404" height="404" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><?php }?>
+            </div>
+          </div>
+        </div>
+        <div class="contact-address-details container-fluid bg-gray">
+          <div class="container">
+          <div class="row">
+            <?php $rows = get_field('address_and_phone_details');
+                if($rows){
+                foreach($rows as $row) { ?>
+                <div class=" details">
+                  <?php if($row['heading']){?><h3 class="active-color"><?php echo $row['heading']?></h3><?php }?>
+                  <?php if($row['details']){?><p><?php echo $row['details']?></p><?php }?>
+                </div>
+            <?php } } ?>  
+            </div>
+            <div class="rounded-circle text-center mobile-map">
+            <?php if(get_field('map_source_link')){?><iframe class="rounded-circle" src="<?php echo get_field('map_source_link');?>" width="404" height="404" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><?php }?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- //Content -->
+
+<?php get_footer();?>
---
+++ wp-content/themes/bangarra/disclaimer-slavery-policy-page.php
@@ -0,0 +1,41 @@
+<?php 
+/**
+ * Template name: Disclaimer Slavery Policy
+ */
+get_header();?>
+
+    <!-- anonymous-information -->
+    <div class="content-main">
+      <div class="common-header-strip">
+        <div class="container">
+        <?php if(get_field('page_title')){?>
+          <h1 class="banner-heading text-white"><?php echo get_field('page_title');?></h1>
+        <?php }?>
+        </div>
+      </div>
+      <div class="anonymous-information">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-7">
+                <?php $content_data = get_field('content');
+                    if($content_data){
+                        foreach($content_data as $data){?>
+                        <?php if($data['heading']){ ?>
+                            <h1 class="primary-color common-primary-heading"><?php echo $data['heading']; ?></h1>
+                        <?php } ?>
+                        <?php if($data['description']){echo $data['description'];} ?>        
+                <?php } } ?>
+            </div>
+            <div class="col-12 col-md-5 d-none d-md-block rounded-circle text-end mt-5 pt-5">
+            <?php if(get_field('image')){?>
+                <img src="<?php echo get_field('image');?>" alt="" class="img-fluid mt-3 pt-4">
+            <?php }?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- //anonymous-information -->
+
+
+<?php get_footer();?>
